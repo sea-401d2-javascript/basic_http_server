d44d4ce1fd07c1c38add4024d1611b0484a8cb13
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-03-09 12:22:39 -0800

    updated package.json and test

--- brandon-feinstein/test/http-test.js
+++ brandon-feinstein/test/http-test.js
@@ -25,4 +25,14 @@ describe('Vanilla HTTP server tests', () => {
         done();
       })
   })
+  it('should return "Hello, Brandon" with POST request', (done) => {
+    request('localhost:3000')
+    .post('/greet')
+    .send({'name': 'Brandon'})
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res.text).to.eql('"Hello, Brandon"');
+      done();
+    });
+  });
 })
