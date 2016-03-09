b7db91f6ff182ed7a224d3e210226fcd5146fdfb
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-09 12:05:02 -0800

    add server changes

--- Samantha/server.js
+++ Samantha/server.js
@@ -5,7 +5,6 @@ var server = http.createServer((req, res) => {
   var u = req.url.split('/');
   var name = u[2];
 
-
   if (req.method === 'GET' && req.url === '/time') {
     var currenttime = new Date().toLocaleTimeString();
     res.writeHead(200, {'content-type': 'text/html'})
@@ -20,21 +19,20 @@ var server = http.createServer((req, res) => {
   }
 
   if(req.method === 'POST' && req.url === '/greet') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
     req.on('data', (data) => {
       var namePost = JSON.parse(data);
       res.write(JSON.stringify(namePost));
-    })
+    });
     req.on('end', () => {
       return res.end()
     });
-    // return res.end();
-  } else {
 
-  res.writeHead(404, {'content-type': 'text/html'});
-  res.write('404 Not Found');
-  res.end();
-}
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'});
+    res.write('404 Not Found');
+    res.end();
+  }
 }).listen(3000, () => console.log('server up on 3000'));
 
   // if (req.method === 'GET' && req.url === '/') {
