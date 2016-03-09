f467cc9ea9f9506a9b4f7afb2b3db119c5f3f8e8
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-09 11:40:42 -0800

    successfully fulfilled all server route assignments. currently writing tests

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ var server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(String(date));
     return res.end();
-  }
-
-  if (req.method === 'GET' && req.url.indexOf('/greet/') > -1) {
+  } else if (req.method === 'GET' && req.url.indexOf('/greet/') > -1) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     var thisUrl = url.parse(req.url);
     console.log(thisUrl);
     var thisUrlPath = String(thisUrl.pathname);
@@ -22,15 +21,19 @@ var server = http.createServer((req, res) => {
     var thisUrlName = thisUrlPath.match(/\Wgreet\/(\w+)/);
     console.log('url name is:', thisUrlName[1]);
     res.write(JSON.stringify(thisUrlName[1] + ', hello. I see you there.'));
+    return res.end();
+  } else if (req.method === 'POST' && req.url === '/greet')
+  {
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    req.on('data', (data) => {
+      var jsonParse = String(data);
+      console.log(jsonParse);
+      res.write(jsonParse);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.write('404 whatever you were looking for was  not found. Sorry');
     res.end();
   }
-
-  if (req.method === 'POST' && req.url === '/greet') {
-    res.writeHead(200, {'Content-Type': 'application/json'})
-
-  }
-
-  res.writeHead(404, {'Content-Type': 'application/json'})
-  res.write('404 whatever you were looking for was not found. Sorry');
-  res.end();
 }).listen(3000, () => console.log('server is up on 3000'));
--- test/test.js
+++ test/test.js
@@ -7,8 +7,26 @@ require(__dirname + '/../server.js');
 var fs = require('fs');
 
 describe('http server tests', () => {
-  it('should respond to /time with a date/time'), (done) => {
-    request('localhost:3000');
-    .get('')
-  }
-})
+  it('should respond to /time with a date/time', (done) => {
+    request('localhost:3000')
+    .get('/time')
+    .end((err, res) => {
+      expect(typeof res.body).to.equal(Date());
+      done();
+    });
+  });
+  it('should return 404 when an incorrect path is entered', (done) => {
+    request('localhost:3000').get('ewersdfaf').end((err, res) => {
+      expect(res.status).to.equal(404);
+      done();
+    });
+  });
+  it('should return name hello. I see you there when accessing greet/name', (done) => {
+    request('localhost:3000')
+    .get('greet/name')
+    .end((err, res) => {
+      expect(res).to.equal('name, hello. I see you there.');
+      done();
+    })
+  })
+});
