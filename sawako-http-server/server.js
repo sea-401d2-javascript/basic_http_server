c0de10b373c07358d0721c6da6f2a668266d3b2c
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-09 08:08:21 -0800

    taking /greet/name url and showing up greeting with the name on my browser

--- sawako-http-server/server.js
+++ sawako-http-server/server.js
@@ -4,9 +4,10 @@ var http = require('http');
 var currentTime;
 
 http.createServer((req,res) =>{
-  var id = req.url.param;
+  var u = req.url.split('/');
+  var id = u[2];
+  console.log('/greet/' + id);
   timeStamp();
-
 
   if(req.url === '/time'){
     res.writeHead(200,'Content-Type: test/html');
@@ -16,7 +17,7 @@ http.createServer((req,res) =>{
 
   if(req.url === '/greet/' + id){
     res.writeHead(200,'Content-Type: test/html');
-    res.write(id);
+    res.write('<p> Hello, ' + id + '! </p>');
     res.end();
   }
 }).listen(3000, ()=> console.log('Port 3000 started!'));
